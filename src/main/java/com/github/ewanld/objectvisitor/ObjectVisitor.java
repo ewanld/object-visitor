c2edb26dd45f97e9a8fdd5df813d5043dcd0d61b
Ewan Lassudrie-Duchesne <ewan.lassudrieduchesne@atos.net>
2018-07-06 10:38:05 +0200

    add options + handle enums separately

--- src/main/java/com/github/ewanld/objectvisitor/ObjectVisitor.java
+++ src/main/java/com/github/ewanld/objectvisitor/ObjectVisitor.java
@@ -2,12 +2,15 @@ package com.github.ewanld.objectvisitor;
 
 import java.lang.reflect.AccessibleObject;
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
@@ -57,21 +60,26 @@ public abstract class ObjectVisitor {
 	private int nestingLevel = 0;
 
 	// filter options
+	private boolean fieldsIncluded = true;
 	private boolean transientFieldsIncluded;
 	private boolean staticFieldsIncluded;
 	private boolean nullsIncluded;
 	private Predicate<Field> fieldInclusionPredicate;
 	private Predicate<Class<?>> classInclusionPredicate;
+	private boolean gettersIncluded;
 
 	// sort options
-	private boolean fieldsSorted = true;
-	private boolean mapEntriesSorted = true;
+	private boolean keysSorted = true;
 	private boolean setsSorted = true;
 
 	// cycle-related options
 	private boolean detectCycles = true;
 	private final List<Object> parentObjects = new ArrayList<>();
 	private Function<Object, Object> alreadyVisitedReplacementFunction = null;
+
+	// transformations options
+	public Function<Field, String> fieldNameFunction = Field::getName;
+	public Function<Method, String> getterNameFunction = Method::getName;
 
 	public enum KeyValueObjectType {
 		MAP, OBJECT
@@ -99,6 +107,8 @@ public abstract class ObjectVisitor {
 
 	public abstract void visitString(String s) throws Exception;
 
+	public abstract void visitEnum(Enum<?> e) throws Exception;
+
 	public abstract void visitNull() throws Exception;
 
 	@SuppressWarnings("unchecked")
@@ -110,6 +120,7 @@ public abstract class ObjectVisitor {
 		addTypeAdapter(Date.class, Object::toString);
 		addTypeAdapter(Instant.class, Object::toString);
 		addTypeAdapter(SimpleDateFormat.class, SimpleDateFormat::toPattern);
+		addTypeAdapter(BigDecimal.class, BigDecimal::doubleValue);
 	}
 
 	public Map<Object, Object> mapOf(Object key, Object value) {
@@ -136,9 +147,7 @@ public abstract class ObjectVisitor {
 		return null;
 	}
 
-	public void visitKey(Object key, KeyType type, Object parent) throws Exception {
-
-	}
+	public abstract void visitKey(Object key, KeyType type, Object parent) throws Exception;
 
 	protected void onIterableEvent(VisitEvent event, Iterable<?> list) throws Exception {
 
@@ -198,8 +207,14 @@ public abstract class ObjectVisitor {
 		} else if (o instanceof Void) {
 			visitNull();
 
+		} else if (o instanceof Enum) {
+			visitEnum((Enum<?>) o);
+
 		} else if (o instanceof String) {
 			visitString((String) o);
+
+		} else if (o instanceof Map) {
+			visitMap((Map<?, ?>) o);
 
 		} else if (o instanceof Set) {
 			final Set<?> set = maybeSortSet((Set<?>) o);
@@ -207,9 +222,6 @@ public abstract class ObjectVisitor {
 
 		} else if (o instanceof Iterable) {
 			visitIterable((Iterable<?>) o);
-
-		} else if (o instanceof Map) {
-			visitMap((Map<?, ?>) o);
 
 		} else if (o instanceof boolean[]) {
 			visitIterable(Arrays.asList(ArrayUtil.boxArray((boolean[]) o)));
@@ -276,40 +288,107 @@ public abstract class ObjectVisitor {
 		}
 
 		final Class<?> _class = o.getClass();
-		final Field[] fields = _class.getDeclaredFields();
-		AccessibleObject.setAccessible(fields, true);
-		if (fieldsSorted) Arrays.sort(fields, Comparator.comparing(field -> field.getName().toLowerCase()));
 		nestingLevel++;
 		onKeyValueObjectEvent(VisitEvent.ENTER, KeyValueObjectType.OBJECT, o);
-		boolean first = true;
-		for (int i = 0; i < fields.length; i++) {
-			final Field field = fields[i];
-			if (!isFieldAccepted(field)) continue;
-			final String key = field.getName();
-			// Warning: Field.get(Object) creates wrappers objects for primitive types.
-			Object child = ObjectVisitorUtil.getQuietly(() -> field.get(o));
-			if (!isObjectAccepted(child)) continue;
-			if (detectCycles) {
-				if (containsReference(parentObjects, child)) {
-					if (alreadyVisitedReplacementFunction == null)
-						continue;
-					else child = alreadyVisitedReplacementFunction.apply(child);
+
+		if (fieldsIncluded) {
+			final List<Field> fields = getFields(_class);
+			if (keysSorted)
+				Collections.sort(fields, Comparator.comparing(fieldNameFunction.andThen(String::toLowerCase)));
+			boolean first = true;
+			for (final Field field : fields) {
+				final String key = fieldNameFunction.apply(field);
+				// Warning: Field.get(Object) creates wrappers objects for primitive types.
+				Object child = ObjectVisitorUtil.getQuietly(() -> field.get(o));
+				if (!isObjectAccepted(child)) continue;
+				if (detectCycles) {
+					if (containsReference(parentObjects, child)) {
+						if (alreadyVisitedReplacementFunction == null)
+							continue;
+						else child = alreadyVisitedReplacementFunction.apply(child);
+					}
+					parentObjects.add(child);
 				}
-				parentObjects.add(child);
+				onKeyValueObjectEvent(VisitEvent.BEFORE_CHILD, KeyValueObjectType.OBJECT, o);
+				if (!first) onKeyValueObjectEvent(VisitEvent.INBETWEEN_CHILDREN, KeyValueObjectType.OBJECT, o);
+				first = false;
+				visitKey(key, KeyType.OBJECT_FIELD, o);
+				visitRecursively(child);
+				onKeyValueObjectEvent(VisitEvent.AFTER_CHILD, KeyValueObjectType.OBJECT, o);
+				if (detectCycles) {
+					final Object popped = parentObjects.remove(parentObjects.size() - 1);
+					assert popped == child;
+				}
 			}
-			onKeyValueObjectEvent(VisitEvent.BEFORE_CHILD, KeyValueObjectType.OBJECT, o);
-			if (!first) onKeyValueObjectEvent(VisitEvent.INBETWEEN_CHILDREN, KeyValueObjectType.OBJECT, o);
-			first = false;
-			visitKey(key, KeyType.OBJECT_FIELD, o);
-			visitRecursively(child);
-			onKeyValueObjectEvent(VisitEvent.AFTER_CHILD, KeyValueObjectType.OBJECT, o);
-			if (detectCycles) {
-				final Object popped = parentObjects.remove(parentObjects.size() - 1);
-				assert popped == child;
+		}
+
+		if (gettersIncluded) {
+			final List<Method> getters = getGetters(_class);
+			if (keysSorted)
+				Collections.sort(getters, Comparator.comparing(getterNameFunction.andThen(String::toLowerCase)));
+			boolean first = true;
+			for (final Method getter : getters) {
+				final String key = getterNameFunction.apply(getter);
+				// Warning: Field.get(Object) creates wrappers objects for primitive types.
+				Object child = null;
+				try {
+					child = getter.invoke(o);
+				} catch (final Exception e) {
+					e.printStackTrace();
+				}
+				if (!isObjectAccepted(child)) continue;
+				if (detectCycles) {
+					if (containsReference(parentObjects, child)) {
+						if (alreadyVisitedReplacementFunction == null)
+							continue;
+						else child = alreadyVisitedReplacementFunction.apply(child);
+					}
+					parentObjects.add(child);
+				}
+				onKeyValueObjectEvent(VisitEvent.BEFORE_CHILD, KeyValueObjectType.OBJECT, o);
+				if (!first) onKeyValueObjectEvent(VisitEvent.INBETWEEN_CHILDREN, KeyValueObjectType.OBJECT, o);
+				first = false;
+				visitKey(key, KeyType.OBJECT_GETTER, o);
+				visitRecursively(child);
+				onKeyValueObjectEvent(VisitEvent.AFTER_CHILD, KeyValueObjectType.OBJECT, o);
+				if (detectCycles) {
+					final Object popped = parentObjects.remove(parentObjects.size() - 1);
+					assert popped == child;
+				}
 			}
 		}
 		nestingLevel--;
 		onKeyValueObjectEvent(VisitEvent.LEAVE, KeyValueObjectType.OBJECT, o);
+	}
+
+	private final List<Field> getFields(Class<?> _class) {
+		final Field[] fields = _class.getDeclaredFields();
+		AccessibleObject.setAccessible(fields, true);
+		final List<Field> res = new ArrayList<>();
+		for (final Field f : fields) {
+			if (isFieldAccepted(f)) res.add(f);
+		}
+		return res;
+	}
+
+	private List<Method> getGetters(Class<?> _class) {
+		final List<Method> res = new ArrayList<>();
+		final Method[] methods = _class.getDeclaredMethods();
+		for (final Method m : methods) {
+			boolean b = true;
+			b &= m.getParameterCount() == 0;
+			b &= !m.getReturnType().equals(Void.TYPE);
+			b &= Modifier.isPublic(m.getModifiers());
+			b &= !Modifier.isStatic(m.getModifiers());
+			b &= !m.getName().equals("getClass");
+			b &= !m.getName().equals("getClasses");
+			b &= !m.getName().equals("getModifiers");
+			b &= m.getName().startsWith("get");
+			if (b) {
+				res.add(m);
+			}
+		}
+		return res;
 	}
 
 	private static <T> TreeSet<T> newTreeSet(Collection<? extends T> collection, Comparator<? super T> comparator) {
@@ -329,7 +408,7 @@ public abstract class ObjectVisitor {
 				return Objects.compare(a.toString(), b.toString(), Comparator.naturalOrder());
 			}
 		};
-		final Set<?> keySet = mapEntriesSorted ? newTreeSet(map.keySet(), comparator) : map.keySet();
+		final Set<?> keySet = keysSorted ? newTreeSet(map.keySet(), comparator) : map.keySet();
 		for (final Object key : keySet) {
 			Object child = map.get(key);
 			if (!isObjectAccepted(child)) continue;
@@ -398,20 +477,13 @@ public abstract class ObjectVisitor {
 	}
 
 	/**
-	 * Specify whether fields should be sorted by their field name.
-	 */
-	public void setFieldsSorted(boolean fieldsSorted) {
-		this.fieldsSorted = fieldsSorted;
-	}
-
-	/**
-	 * Specify whether map entries should be sorted according to their key.
+	 * Specify whether keys (map keys, object fields, and object getters) should be sorted.
 	 * <p>
 	 * When set to {@code true}, keys are sorted by their natural order if keys implement the {@link Comparable}
 	 * interface, or according to their {@code toString} representation otherwise.
 	 */
-	public void setMapEntriesSorted(boolean mapEntriesSorted) {
-		this.mapEntriesSorted = mapEntriesSorted;
+	public void setKeysSorted(boolean keysSorted) {
+		this.keysSorted = keysSorted;
 	}
 
 	/**
@@ -449,6 +521,13 @@ public abstract class ObjectVisitor {
 	 */
 	public void setStaticFieldsIncluded(boolean staticFieldsIncluded) {
 		this.staticFieldsIncluded = staticFieldsIncluded;
+	}
+
+	/**
+	 * Specify whether {@code static} class fields should be included/excluded.
+	 */
+	public void setFieldsIncluded(boolean fieldsIncluded) {
+		this.fieldsIncluded = fieldsIncluded;
 	}
 
 	/**
@@ -498,12 +577,8 @@ public abstract class ObjectVisitor {
 		return classInclusionPredicate;
 	}
 
-	public boolean isFieldsSorted() {
-		return fieldsSorted;
-	}
-
-	public boolean isMapEntriesSorted() {
-		return mapEntriesSorted;
+	public boolean isKeysSorted() {
+		return keysSorted;
 	}
 
 	public boolean isSetsSorted() {
@@ -516,5 +591,33 @@ public abstract class ObjectVisitor {
 
 	public Function<Object, Object> getAlreadyVisitedReplacementFunction() {
 		return alreadyVisitedReplacementFunction;
+	}
+
+	public boolean isGettersIncluded() {
+		return gettersIncluded;
+	}
+
+	public void setGettersIncluded(boolean gettersIncluded) {
+		this.gettersIncluded = gettersIncluded;
+	}
+
+	public boolean isFieldsIncluded() {
+		return fieldsIncluded;
+	}
+
+	public Function<Field, String> getFieldNameFunction() {
+		return fieldNameFunction;
+	}
+
+	public void setFieldNameFunction(Function<Field, String> fieldNameFunction) {
+		this.fieldNameFunction = fieldNameFunction;
+	}
+
+	public Function<Method, String> getGetterNameFunction() {
+		return getterNameFunction;
+	}
+
+	public void setGetterNameFunction(Function<Method, String> getterNameFunction) {
+		this.getterNameFunction = getterNameFunction;
 	}
 }
